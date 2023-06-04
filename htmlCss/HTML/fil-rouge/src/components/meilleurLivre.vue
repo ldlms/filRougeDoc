18cd637022314c13d86cfcabade6ae4f1fa3f54b
leo <leodelmas31@gmail.com>
2023-06-04 17:51:15 +0200

    0406

---
+++ htmlCss/HTML/fil-rouge/src/components/meilleurLivre.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="ftp">
+  <img class="livreTopFlop" src="./assets/L-Etranger.jpg" alt="bouquin">
+  <p class="annonce">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam iaculis leo nunc. Etiam sagittis erat at turpis venenatis, eget porttitor massa convallis....</p>
+  <div class="note"><h1>10</h1></div>
+  <button class="button-18" role="button">Voir les moins bonnes notes</button>
+</div>
+</template>
+
+<style>
+.container {
+  height: 100%;
+  display: grid; 
+  grid-template-columns: 1fr 1fr 1fr; 
+  grid-template-rows: 1.3fr 1.3fr 0.4fr; 
+  gap: 0px 0px; 
+  grid-template-areas: 
+    "top top top"
+    "carousel carousel carousel"
+    "navBar navBar navBar"; 
+}
+.navBar{
+  grid-area: navBar;
+  background-color: #336DBF;
+  display: grid; 
+  grid-template-columns: 1fr 1fr 1fr 1fr; 
+  gap: 0px 0px; 
+  grid-template-areas: 
+    ". . . ."; 
+}
+.ftp{
+  grid-area: top;
+  display: grid; 
+  grid-template-columns: 1.3fr 0.9fr 0.8fr; 
+  grid-template-rows: 2fr 1fr 0.5fr; 
+  gap: 0px 0px; 
+  grid-template-areas: 
+    "livre textetop textetop"
+    "livre note ."
+    "nav nav nav";  
+}
+.menuD{
+  grid-area: carousel;
+}
+.livreTopFlop{
+  grid-area: livre;
+  margin-left: 13px;
+  margin-top: 45px;
+}
+.annonce{
+  grid-area: textetop;
+  margin-left: 10px;
+  margin-top: 45px;
+}
+.note{
+  border-radius: 50%;
+  background-color: #38BD36;
+  grid-template-areas:note;
+  height: 70px;
+  width: 70px;
+  text-align: center;
+  margin:13px;
+  box-shadow:2px 5px 16px 0px #0B325E;
+}
+  .button-18 {
+  grid-area: nav;
+  align-items: center;
+  background-color: #336DBF;
+  border: 0;
+  border-radius: 100px;
+  box-sizing: border-box;
+  color: #ffffff;
+  cursor: pointer;
+  display: inline-flex;
+  font-family: -apple-system, system-ui, system-ui, "Segoe UI", Roboto, "Helvetica Neue", "Fira Sans", Ubuntu, Oxygen, "Oxygen Sans", Cantarell, "Droid Sans", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Lucida Grande", Helvetica, Arial, sans-serif;
+  font-size: 16px;
+  font-weight: 600;
+  justify-content: center;
+  line-height: 20px;
+  max-width: 480px;
+  min-height: 40px;
+  min-width: 0px;
+  overflow: hidden;
+  padding: 0px;
+  padding-left: 20px;
+  padding-right: 20px;
+  margin-left: 15%;
+  margin-right: 15%;
+  text-align: center;
+  touch-action: manipulation;
+  transition: background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s;
+  user-select: none;
+  -webkit-user-select: none;
+  vertical-align: middle;
+}
+
+.button-18:hover,
+.button-18:focus { 
+  background-color: #16437E;
+  color: #ffffff;
+}
+
+.button-18:active {
+  background: #09223b;
+  color: rgb(255, 255, 255, .7);
+}
+
+.button-18:disabled { 
+  cursor: not-allowed;
+  background: rgba(0, 0, 0, .08);
+  color: rgba(0, 0, 0, .3);
+}
+</style>
---
+++ htmlCss/HTML/fil-rouge/src/components/menuDeroulant.vue
@@ -0,0 +1,99 @@
+<template>
+<template>
+  <div class="scrollable-container">
+    <div class="scrollable" ref="scrollable">
+      <div class="content">
+        <img src="image1.jpg" alt="Image 1">
+        <img src="image2.jpg" alt="Image 2">
+        <img src="image3.jpg" alt="Image 3">
+        <img src="image4.jpg" alt="Image 4">
+        <img src="image5.jpg" alt="Image 5">
+        <img src="image6.jpg" alt="Image 6">
+      </div>
+    </div>
+    <button class="scroll-btn prev-btn" @click="scrollLeft"><span>&lt;</span></button>
+    <button class="scroll-btn next-btn" @click="scrollRight"><span>&gt;</span></button>
+  </div>
+</template>
+
+</template>
+
+
+
+<script>
+export default{
+methods: {
+  scrollLeft() {
+    const scrollable = this.$refs.scrollable;
+    scrollable.scrollLeft -= 200;
+  },
+  scrollRight() {
+    const scrollable = this.$refs.scrollable;
+    scrollable.scrollLeft += 200;
+  }
+}
+}
+</script>
+
+
+<style>
+.scrollable-container {
+  position: relative;
+  width: 100%;
+  height: 400px;
+  overflow: hidden;
+}
+
+.scrollable {
+  display: flex;
+  flex-wrap: nowrap;
+  width: max-content;
+  height: 100%;
+  padding: 10px;
+  margin-right: 70px;
+}
+
+.content {
+  display: flex;
+  flex-wrap: nowrap;
+  width: fit-content;
+  height: 100%;
+}
+
+.scrollable img {
+  width: 400px;
+  height: 400px;
+  margin-right: 20px;
+}
+
+.scroll-btn {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: none;
+  background-color: rgba(255, 255, 255, 0.5);
+  opacity: 0;
+  transition: opacity 0.2s ease-in-out;
+}
+
+.scroll-btn span {
+  display: inline-block;
+  font-size: 30px;
+  line-height: 1;
+}
+
+.scroll-btn:hover {
+  opacity: 1;
+}
+
+.prev-btn {
+  left: 0;
+}
+
+.next-btn {
+  right: 0;
+}
+</style>
